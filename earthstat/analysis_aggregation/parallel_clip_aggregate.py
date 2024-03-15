d31df524b457267e47d728ef1ffa2696cf82bf9a
AbdelrahmanAmr3 <abdulrahman.amr.ali@gmail.com>
2024-03-15 12:11:16 +0300

    Enhance Parallel Aggregation and Add Aggregation Options

--- earthstat/analysis_aggregation/parallel_clip_aggregate.py
+++ earthstat/analysis_aggregation/parallel_clip_aggregate.py
@@ -11,89 +11,82 @@ from shapely.geometry import mapping
 from ..utils import extractDateFromFilename, loadTiff
 
 
-def process_and_aggregate_raster(raster, shapefile_path, use_crop_mask, crop_mask_path, invalid_values=None, no_data_value=None, predictor_name="Value"):
-    """
-    Process a single raster: clip with shapefile, then aggregate,
-    and include data from each row in the shapefile in the output.
-    """
-    file_name = os.path.basename(raster)
+def process_and_aggregate_raster(raster_path, shape_file, invalid_values=None, use_mask=False, mask_path=None, calculation_mode="overall_mean", predictor_name="Value", all_touched=False):
+
+    file_name = os.path.basename(raster_path)
     date_str = extractDateFromFilename(file_name)
+    aggregated_data = []
+
+    with rasterio.open(raster_path) as src:
+        no_data_value = src.nodata
+        geoms = [mapping(shape) for shape in shape_file.geometry]
+
+        with rasterio.open(mask_path) as mask_src:
+            mask_no_data_value = mask_src.nodata
+
+            for index, geom in enumerate(geoms):
+                geom_mask, geom_transform = mask(
+                    src, [geom], crop=True, all_touched=all_touched)
+                geom_mask = geom_mask.astype('float32')
+
+                geom_mask[geom_mask == no_data_value] = np.nan
+                if invalid_values:
+                    for invalid_value in invalid_values:
+                        geom_mask[geom_mask == invalid_value] = np.nan
+
+                if use_mask and mask_path:
+                    if calculation_mode == "weighted_mean":
+                        crop_mask, _ = mask(
+                            mask_src, [geom], crop=True, all_touched=all_touched)
+                        valid_mask = (crop_mask[0] != mask_no_data_value)
+                        valid_data = geom_mask[0][valid_mask]
+                        valid_weights = crop_mask[0][valid_mask]
+                        mean_value = np.nansum(valid_data * valid_weights) / np.nansum(
+                            valid_weights) if np.nansum(valid_weights) > 0 else np.nan
+
+                    elif calculation_mode == "filtered_mean":
+                        crop_mask, _ = mask(
+                            mask_src, [geom], crop=True, all_touched=all_touched)
+                        valid_mask = (crop_mask[0] != mask_no_data_value)
+                        masked_data = geom_mask[0][valid_mask]
+                        mean_value = np.nanmean(masked_data) if np.nansum(
+                            masked_data) > 0 else np.nan
+
+                    else:  # defualt "overall_mean"
+                        mean_value = np.nanmean(geom_mask)
+
+                new_row = {col: shape_file.iloc[index][col]
+                           for col in shape_file.columns if col != 'geometry'}
+                new_row.update(
+                    {'date': date_str, predictor_name: mean_value})
+                aggregated_data.append(new_row)
+
+    return aggregated_data
+
+
+def parallelAggregate(predictor_dir, shapefile_path, output_csv_path, mask_path=None, use_mask=False, invalid_values=None, calculation_mode="overall_mean", predictor_name="Value", all_touched=False):
+    # Load TIFF files from the provided directory
+    predictor_paths = loadTiff(predictor_dir)
     data_list = []
 
     shape_file = gpd.read_file(shapefile_path)
 
-    with rasterio.open(raster) as src:
-        shape_file_crs = shape_file.to_crs(src.crs)
-        geoms = [mapping(shape) for shape in shape_file_crs.geometry]
+    # Ensure mask_path is provided if use_mask is True
+    if use_mask and not mask_path:
+        raise ValueError("Mask path must be provided if use_mask is True.")
 
-        for index, geom in enumerate(geoms):
-            geom_mask, geom_transform = mask(
-                src, [geom], crop=True, all_touched=True)
-            # Ensure data type consistency for NaN handling
-            geom_mask = geom_mask.astype('float32')
+    # Use ProcessPoolExecutor for parallel processing
+    with ProcessPoolExecutor(max_workers=os.cpu_count()) as executor:
+        # Create a list of futures for the executor to process
+        futures = [executor.submit(process_and_aggregate_raster, raster_path, shape_file, invalid_values,
+                                   use_mask, mask_path, calculation_mode, predictor_name, all_touched) for raster_path in predictor_paths]
 
-            if invalid_values is not None:
-                for invalid_value in invalid_values:
-                    geom_mask[geom_mask == invalid_value] = np.nan
-
-            if use_crop_mask and crop_mask_path:
-                with rasterio.open(crop_mask_path) as mask_src:
-                    crop_mask, _ = mask(
-                        mask_src, [geom], crop=True, all_touched=True)
-                if no_data_value is not None:
-                    valid_mask = (crop_mask[0] != no_data_value)
-                else:
-                    valid_mask = np.ones_like(crop_mask[0], dtype=bool)
-
-                valid_data = geom_mask[0][valid_mask]
-                valid_weights = crop_mask[0][valid_mask]
-
-                if valid_weights.sum() > 0:
-                    weighted_mean = np.nansum(
-                        valid_data * valid_weights) / np.nansum(valid_weights)
-                else:
-                    weighted_mean = np.nan
-            else:
-                valid_data = geom_mask[0][~np.isnan(geom_mask[0])]
-                if valid_data.size > 0:
-                    weighted_mean = np.nanmean(valid_data)
-                else:
-                    weighted_mean = np.nan
-
-            # Create a new row with shapefile data and aggregated value
-            row = shape_file.iloc[index]
-            new_row = {col: row[col]
-                       for col in shape_file.columns if col != 'geometry'}
-            new_row.update({
-                'date': date_str,
-                predictor_name: np.round(weighted_mean, 3)
-            })
-            data_list.append(new_row)
-
-    return data_list
-
-
-def parallelAggregate(predictor_dir, shapefile_path, output_csv_path, crop_mask_path=None, use_crop_mask=False, invalid_values=None, predictor_name="Value"):
-    predictor_paths = loadTiff(predictor_dir)
-    data_list = []
-
-    if use_crop_mask and not crop_mask_path:
-        raise ValueError(
-            "Crop mask path must be provided if use_crop_mask is true.")
-
-    if use_crop_mask:
-        mask_src = rasterio.open(crop_mask_path)
-        no_data_value = mask_src.nodata
-        mask_src.close()
-    else:
-        no_data_value = None
-
-    with ProcessPoolExecutor() as executor:
-        futures = [executor.submit(process_and_aggregate_raster, raster, shapefile_path, use_crop_mask,
-                                   crop_mask_path, invalid_values, no_data_value, predictor_name) for raster in predictor_paths]
-
+        # Process futures as they complete
         for future in tqdm(as_completed(futures), total=len(futures), desc="Processing rasters", unit="raster"):
+            # Aggregate results from futures
             data_list.extend(future.result())
 
+    # Convert the aggregated data into a DataFrame and save to CSV
     df = pd.DataFrame(data_list)
+    df[predictor_name] = df[predictor_name].round(3)
     df.to_csv(output_csv_path, index=False)

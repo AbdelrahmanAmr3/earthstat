04a07b38c9a6d27e42eb4b04be212aeddc7fd558
AbdelrahmanAmr3 <abdulrahman.amr.ali@gmail.com>
2024-03-16 11:29:46 +0300

    Add documentation for using modules seperatlly #25

--- earthstat/geo_meta_extractors/shapefile_meta.py
+++ earthstat/geo_meta_extractors/shapefile_meta.py
@@ -4,6 +4,18 @@ from pyproj import CRS
 
 
 def shapefileMeta(shapefile_path):
+    """
+    Summarizes key metadata of a shapefile, including geometry types, CRS, extent,
+    feature count, and attribute names. Assumes the shapefile can be read using
+    GeoPandas.
+
+    Args:
+        shapefile_path (str): Path to the shapefile.
+
+    Returns:
+        dict: Contains 'Geometry Type', 'Coordinate Reference System (CRS)', 'Extent',
+              'Feature Count', and 'Attributes' of the shapefile.
+    """
     # Load the shapefile
     gdf = gpd.read_file(shapefile_path)
     crs = CRS(gdf.crs).name

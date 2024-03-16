d6a4ac8bec7f0090390627bee2cd5249f6affc65
AbdelrahmanAmr3 <abdulrahman.amr.ali@gmail.com>
2024-03-16 14:21:21 +0300

    Add documentation #25

--- docs/utils.md
+++ docs/utils.md
@@ -1,3 +1,7 @@
-# Utils module
+# Earth's Utilities
 
+Detailed documentation on the `earthstat.utils` module, a collection of functions designed usually used through building the EarthStat library. 
+
+
+## Utilities Overview
 ::: earthstat.utils
--- earthstat/utils.py
+++ earthstat/utils.py
@@ -5,8 +5,14 @@ import glob
 
 
 def savedFilePath(file_path):
-    """Get file directory and name.
+    """
+    Extracts and returns the directory and name of a file from its path.
 
+    Args:
+        file_path (str): The full path to the file.
+
+    Returns:
+        tuple: A tuple containing the file's directory and name.
     """
     file_dir = os.path.dirname(file_path)
     file_name = os.path.basename(file_path)
@@ -15,6 +21,15 @@ def savedFilePath(file_path):
 
 
 def extractDateFromFilename(filename):
+    """
+    Extracts a date string in 'YYYYMMDD' format from a filename.
+
+    Args:
+        filename (str): The name of the file containing a date.
+
+    Returns:
+        str: The extracted date string.
+    """
     pattern = r'\d{8}'
     match = re.search(pattern, filename)
     date_str = match.group()
@@ -22,9 +37,27 @@ def extractDateFromFilename(filename):
 
 
 def convertDate(date):
+    """
+    Converts a date string from 'YYYYMMDD' format to a date object.
+
+    Args:
+        date (str): The date string in 'YYYYMMDD' format.
+
+    Returns:
+        datetime.date: The corresponding date object.
+    """
     return datetime.strptime(date, '%Y%m%d').date()
 
 
 def loadTiff(directory):
+    """
+    Loads and returns paths to all TIFF files in a specified directory.
+
+    Args:
+        directory (str): The directory to search for TIFF files.
+
+    Returns:
+        list: A list of paths to the TIFF files found in the directory.
+    """
     paths = glob.glob(directory+'/*.tif')
     return paths

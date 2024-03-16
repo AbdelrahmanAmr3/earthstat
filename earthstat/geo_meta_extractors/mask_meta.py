180ff6a5e574b5354b1068c28faa875e9641469e
AbdelrahmanAmr3 <abdulrahman.amr.ali@gmail.com>
2024-03-16 11:22:34 +0300

    Add documentation for using modules seperatlly #25

--- earthstat/geo_meta_extractors/mask_meta.py
+++ earthstat/geo_meta_extractors/mask_meta.py
@@ -3,13 +3,30 @@ from pyproj import CRS
 
 
 def maskSummary(raster_path):
-    """_summary_
+    """
+    Generates a summary of essential information for a raster mask file.
 
-    Args:
-        raster_path (_type_): _description_
+    This function opens a raster file, reads its first band with masking enabled to handle nodata values, and then computes and extracts several key pieces of information about the raster. These include the coordinate reference system (CRS) name, geographical extent, data type of the raster, nodata value, spatial resolution, pixel size, and the minimum and maximum values within the raster.
+
+    Parameters:
+    - raster_path (str): The file path to the raster file.
 
     Returns:
-        _type_: _description_
+    dict: A dictionary containing the mask path, CRS, geographical extent, data type, nodata value, spatial resolution (as width and height), pixel size, and the minimum and maximum value of the raster's first band.
+
+    Example of use:
+    >>> raster_summary = maskSummary('path/to/raster.tif')
+    >>> print(raster_summary)
+    {
+        'Mask_path': 'path/to/raster.tif',
+        'CRS': 'EPSG:4326',
+        'Extent': BoundingBox(left=0.0, bottom=0.0, right=10.0, top=10.0),
+        'Data Type': 'uint16',
+        'NoData Value': -9999.0,
+        'Spatial Resolution': (1024, 768),
+        'Pixel Size': (0.1, 0.1),
+        'Min/Max Value': (0, 255)
+    }
     """
     with rasterio.open(raster_path) as src:
 

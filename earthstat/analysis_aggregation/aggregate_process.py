3f01a453b19fbf97e00a30d4617fa8dc778df06e
AbdelrahmanAmr3 <abdulrahman.amr.ali@gmail.com>
2024-03-18 18:21:10 +0300

    add doc

--- earthstat/analysis_aggregation/aggregate_process.py
+++ earthstat/analysis_aggregation/aggregate_process.py
@@ -85,7 +85,24 @@ def process_and_aggregate_raster(raster_path, shape_file, invalid_values=None, u
 
 
 def conAggregate(predictor_dir, shapefile_path, output_csv_path, mask_path=None, use_mask=False, invalid_values=None, calculation_mode="overall_mean", predictor_name="Value", all_touched=False):
+    """
+    Aggregates raster values to polygons in a shapefile, optionally using a crop mask for weighted calculations.
 
+    Args:
+        predictor_dir (str): Directory containing raster datasets.
+        shapefile_path (str): Path to the shapefile with polygons for aggregation.
+        output_csv_path (str): Path where the aggregated output CSV will be saved.
+        crop_mask_path (str, optional): Path to the crop mask raster, required if use_crop_mask is True.
+        use_crop_mask (bool): Whether to use the crop mask for weighted aggregation.
+        predictor_name (str): Column name for the aggregated values in the output CSV.
+
+    Raises:
+        ValueError: If use_crop_mask is True but crop_mask_path is not provided.
+
+    Aggregates values from each raster within the specified directory to each polygon in the shapefile,
+    writing the results to a CSV file. If a crop mask is used, values are aggregated using weights from
+    the mask; otherwise, simple averaging is applied.
+    """
     predictor_paths = loadTiff(predictor_dir)
     data_list = []
 
@@ -103,87 +120,3 @@ def conAggregate(predictor_dir, shapefile_path, output_csv_path, mask_path=None,
     df = pd.DataFrame(data_list)
     df[predictor_name] = df[predictor_name].round(3)
     df.to_csv(output_csv_path, index=False)
-
-
-# def Aggregate(predictor_dir, shapefile_path, output_csv_path, crop_mask_path=None, use_crop_mask=False, predictor_name="Value"):
-
-#     predictor_pathes = loadTiff(predictor_dir)
-#     data_list = []
-#     shape_file = gpd.read_file(shapefile_path)
-
-#     if use_crop_mask:
-#         if crop_mask_path is None:
-#             raise ValueError(
-#                 "Crop mask path must be provided if use_crop_mask is True.")
-#         mask_src = rasterio.open(crop_mask_path)
-#         # Dynamically read the no data value from the crop mask metadata
-#         no_data_value = mask_src.nodata
-
-#     for raster in tqdm(predictor_pathes, desc="Processing rasters", unit="raster"):
-#         file_name = os.path.basename(raster)
-#         date_str = extractDateFromFilename(file_name)
-
-#         with rasterio.open(raster) as src:
-#             for _, row in shape_file.iterrows():
-#                 geom = row.geometry
-#                 out_image, _ = mask(src, [geom], crop=True)
-
-#                 if use_crop_mask:
-#                     mask_image, _ = mask(mask_src, [geom], crop=True)
-#                     if no_data_value is not None:
-#                         # Mask out no data values in the crop mask
-#                         valid_mask = (mask_image != no_data_value)
-#                     else:
-#                         # Assume all data is valid if no_data_value is not specified
-#                         valid_mask = np.ones_like(mask_image, dtype=bool)
-
-#                     valid_data = out_image[valid_mask]
-#                     valid_weights = mask_image[valid_mask]
-
-#                     if valid_weights.sum() > 0:  # Check to avoid division by zero
-#                         weighted_mean = np.nansum(
-#                             valid_data * valid_weights) / np.nansum(valid_weights)
-#                         # Round to 3 decimal places
-#                         value = np.round(weighted_mean, 3)
-#                     else:
-#                         value = np.nan  # No valid data points
-
-#                 else:
-#                     # Round to 3 decimal places
-#                     value = np.round(np.nanmean(out_image), 3)
-
-#                 new_row = {col: row[col]
-#                            for col in shape_file.columns if col != 'geometry'}
-#                 new_row.update({
-#                     'date': date_str,
-#                     predictor_name: value
-#                 })
-
-#                 data_list.append(new_row)
-
-#     if use_crop_mask:
-#         mask_src.close()
-
-#     df = pd.DataFrame(data_list)
-#     df.to_csv(output_csv_path, index=False)
-
-
-# def conAggregate():
-#     """
-#     Aggregates raster values to polygons in a shapefile, optionally using a crop mask for weighted calculations.
-
-#     Args:
-#         predictor_dir (str): Directory containing raster datasets.
-#         shapefile_path (str): Path to the shapefile with polygons for aggregation.
-#         output_csv_path (str): Path where the aggregated output CSV will be saved.
-#         crop_mask_path (str, optional): Path to the crop mask raster, required if use_crop_mask is True.
-#         use_crop_mask (bool): Whether to use the crop mask for weighted aggregation.
-#         predictor_name (str): Column name for the aggregated values in the output CSV.
-
-#     Raises:
-#         ValueError: If use_crop_mask is True but crop_mask_path is not provided.
-
-#     Aggregates values from each raster within the specified directory to each polygon in the shapefile,
-#     writing the results to a CSV file. If a crop mask is used, values are aggregated using weights from
-#     the mask; otherwise, simple averaging is applied.
-#     """
